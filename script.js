7669e9f05dba96cb0d248d2f8cac222967b5deac
Saravanakumaran08011995 <saravanakumaran8195@gmail.com>
2022-11-24 21:45:24 +0530

    changes

--- script.js
+++ script.js
@@ -29,4 +29,21 @@ for(let i in arr){
 }
 for(let i of arr){
     console.log(i);
-}
+} 
+
+/* 
+Output:
+Day-3 Task Question 1
+name John
+name Doe
+name Ram
+name Lisa
+age 22
+age 21
+age 25
+age 23
+{name: 'John', age: '22', gender: 'Male'}
+{name: 'Doe', age: '21', gender: 'Male'}
+{name: 'Ram', age: '25', gender: 'Male'}
+{name: 'Lisa', age: '23', gender: 'Female'}
+*/
--- script_1.js
+++ script_1.js
@@ -17,3 +17,14 @@ console.log("Skills :",resume.Skills);
 console.log("Certification:",resume.Certification);
 console.log("Experience :",resume.Experience);
 console.log("Achievements :",resume.Achievements);
+
+/* Output:
+Day-3 Task Question 2
+ Name : Aleks Ludkee
+ Mobile : (123)456-7890
+ email : ludkee.alex@gmail.com
+ Education : B.S. Computer Science, University of Tennesse
+ Skills : JavaScript, HTML, CSS, BootStrap, React.js, Node.js, RestAPIs, MangoDB, AWS
+ Certification: MTA, AWS
+ Experience : Jr. Full Stack developer in Randstand Technologies(Jun2019 -Aug2020), Full Stack Developer in Deloitte(Aug2020 - current)
+ Achievements : Designed, developed and modified 25+ software system and custom components */
